6907f34e60bd9b5686ad0774b25c412be8f7f993
anapaulacrippa <aplcrippa@gmail.com>
2026-06-22 22:49:12 -0300

    implementa selecao por torneio; inicia geracao de individuo

--- genetico.c
+++ genetico.c
@@ -4,7 +4,41 @@
 #include "genetico.h"
 
 Solucao gerar_individuo(Instancia *instancia) {
+    int N = instancia->N;
+    int M = instancia->M;
 
+    Solucao solucao;
+    solucao.cromossomo = calloc(N, sizeof(int));
+    solucao.conjunto_colunas = malloc(N * sizeof(int));
+    solucao.num_colunas = 0;
+    solucao.linhas_cobertas = calloc(M, sizeof(int));
+    solucao.num_linhas = 0;
+    solucao.custo_total = 0.0;
+    solucao.avaliacao = 0.0;
+
+    int *descobertas = malloc(M * sizeof(int));
+
+    for (int i = 0; i < M; i++) {
+        descobertas[i] = i;
+    }
+
+    int num_descobertas = M;
+
+    while (num_descobertas > 0) {
+        int linha;
+        int num_candidatas = inst->linha_colunas_tam[linha]
+
+        if (num_candidatas == 0) { // linha impossível
+
+        }
+
+
+        // atualizar lista de linhas descobertas
+    }
+
+    eliminar_redudancia(&solucao, instancia);
+
+    return solucao;
 }
 
 void eliminar_redudancia(Solucao *solucao, Instancia *instancia) {
@@ -18,8 +52,27 @@ Populacao gerar_populacao_inicial(int tamanho, Instancia *instancia) {
 
     for(int i = 0; i < tamanho; i++) {
         populacao.individuos[i] = gerar_individuo(instancia);
-        eliminar_redundancia(&populacao.individuos[i], instancia);
     }
 
     return populacao;
+}
+
+
+/*
+Determina quais indivíduos se reproduzirão para gerar a próxima população.
+Utiliza técnica de torneio com k=TORNEIO candidatos aleatórios.
+Escolhe o mais adaptado (menor avaliação).
+*/
+int selecao(Populacao *populacao) {
+    int melhor = rand() % populacao->tamanho;
+
+    for(int k = 1; k < TORNEIO; k++){
+        int candidato = rand() % populacao->tamanho;
+
+        if(populacao->individuos[candidato].avaliacao < populacao->individuos[melhor].avaliacao) {
+            melhor = candidato;
+        }
+    }
+
+    return melhor;
 }
